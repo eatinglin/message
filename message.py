da6ee8f86e4c69e0650b29181d2b357845410f21
Yiting Lin <bestfriend830@gmail.com>
2021-10-09 16:01:08 +0800

    count <100 word

--- message.py
+++ message.py
@@ -13,11 +13,19 @@ print('檔案讀取完了，總共有', len(data), '筆資料')
 #算留言平均長度
 
 
-
 number = 0
 for word in data:
 	number += len(word)
 print('留言平均長度為', number/len(data))
+
+
+new = []
+for word in data:
+	if len(word) < 100:
+		new.append(word)
+print('共有', len(new), '筆留言小於100字')
+
+
 
 """
 word = 'Hi, new world'
