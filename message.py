bd4c643c9eaeec2ae32058bd98b34d6cf380a1f6
Yiting Lin <bestfriend830@gmail.com>
2021-10-09 15:40:23 +0800

    calc avg length reviews

--- message.py
+++ message.py
@@ -1,3 +1,4 @@
+
 data = []
 count = 0
 
@@ -7,4 +8,20 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
+#算留言平均長度
+
+
+
+number = 0
+for word in data:
+	number += len(word)
+print('留言平均長度為', number/len(data))
+
+"""
+word = 'Hi, new world'
+
+for w in word:
+	print(w)
+"""
